7ca30f6f34fd2445da8e2ad4d005552a997eb377
Manakal <dp.manakal82@gmail.com>
2021-09-14 22:22:12 +0530

    Queries And Mutations Added Using Method 2

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import {ApolloServer} from "apollo-server-express";
-import {schema} from "./graphql/graphql";
+import {resolvers,typeDefs} from "./graphql/index";
 
 const app=express();
 const port=9000;
 
 async function startServer() {
-    const server=new ApolloServer({schema});
+    const server=new ApolloServer({resolvers,typeDefs});
     await server.start();
     server.applyMiddleware({app,path:"/api"});
 }
